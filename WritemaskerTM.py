ad92be1300a5109324d495d2ea57d97507116e5c
Simonrak <72688649+Simonrak@users.noreply.github.com>
2024-08-13 00:52:53 +0200

    Update WritemaskerTM.py
    Wrong script, correct one now.

--- WritemaskerTM.py
+++ WritemaskerTM.py
@@ -1,6 +1,6 @@
 import re
 
-write_protected_bits_PCIE = [
+write_protected_bits_PCIE = (
     "00000f00",  # 1
     "00000010",  # 2
     "ff7f0f00",  # 3
@@ -14,14 +14,14 @@ write_protected_bits_PCIE = [
     "ff7f0000",  # 11
     "00000000",  # 12
     "bfff2000",  # 13
-]
+)
 
-write_protected_bits_PM = [
+write_protected_bits_PM = (
     "00000000",  # 1
     "00000000",  # 2
-]
+)
 
-write_protected_bits_MSI = [
+write_protected_bits_MSI = (
     "0000f104",  # 1
     "03000000",  # 2
     "00000000",  # 3
@@ -30,28 +30,28 @@ write_protected_bits_MSI = [
     "00000000",  # 6
     "00000000",  # 7
     "ffff0000",  # 8
-]
+)
 
-write_protected_bits_MSIX = [
+write_protected_bits_MSIX = (
     "000000c0",  # 1
     "00000000",  # 2
     "00000000",  # 3
-]
+)
 
-write_protected_bits_VSC = [
+write_protected_bits_VSC = (
     "000000ff",  # 1
     "ffffffff",  # 2
     "00000000",  # 3
-]
+)
 
-write_protected_bits_VSEC = [
+write_protected_bits_VSEC = (
     "00000000",  # 1
     "00000000",  # 2
     "ffffffff",  # 3
     "ffffffff",  # 4
-]
+)
 
-write_protected_bits_AER = [
+write_protected_bits_AER = (
     "00000000",  # 1
     "31f0ff07",  # 2
     "31f0ff07",  # 3
@@ -63,32 +63,32 @@ write_protected_bits_AER = [
     "00000000",  # 9
     "00000000",  # 10
     "00000000",  # 11
-]
+)
 
-write_protected_bits_DSN = [
+write_protected_bits_DSN = (
     "00000000",  # 1
     "00000000",  # 2
     "00000000",  # 3
-]
+)
 
-write_protected_bits_LTR = [
+write_protected_bits_LTR = (
     "00000000",  # 1
     "00000000",  # 2
-]
+)
 
-write_protected_bits_L1PM = [
+write_protected_bits_L1PM = (
     "00000000",  # 1
     "00000000",  # 2
     "3f00ffe3",  # 3
     "fb000000",  # 4
-]
+)
 
-write_protected_bits_PTM = [
+write_protected_bits_PTM = (
     "00000000",  # 1
     "00000000",  # 2
     "00000000",  # 3
     "03ff0000",  # 4
-]
+)
 
 CAPABILITY_NAMES = {
     0x01: "power management",
@@ -168,114 +168,91 @@ fixed_section = [
     "01f8ffff", "00000000", "00000000", "ff000000",
 ]
 
-fixed_mask_section = {
-    0x10: write_protected_bits_PCIE,
-    0x01: write_protected_bits_PM,
-    0x05: write_protected_bits_MSI,
-    0x11: write_protected_bits_MSIX,
-    0x09: write_protected_bits_VSC,
-    0x0A: write_protected_bits_VSEC,
-    0x0D: write_protected_bits_AER,
-    0x03: write_protected_bits_DSN,
-    0x0C: write_protected_bits_LTR,
-    0x07: write_protected_bits_L1PM,
-    0x0B: write_protected_bits_PTM,
+writemask_dict = {
+    "0x10": write_protected_bits_PCIE,
+    "0x01": write_protected_bits_PM,
+    "0x05": write_protected_bits_MSI,
+    "0x11": write_protected_bits_MSIX,
+    "0x09": write_protected_bits_VSC,
+    "0x00A": write_protected_bits_VSEC,
+    "0x0001": write_protected_bits_AER,
+    "0x0003": write_protected_bits_DSN,
+    "0x0018": write_protected_bits_LTR,
+    "0x001E": write_protected_bits_L1PM,
+    "0x000B": write_protected_bits_PTM,
 }
 
 
-def parse_pci_config_space(file_path):
-    config_space_map = {}
-    extended_config_space_map = {}
+def read_cfg_space(file_path):
+    dword_map = {}
     index = 0
     with open(file_path, 'r') as file:
         for line in file:
             dwords = re.findall(r'[0-9a-fA-F]{8}', line.strip())
             for dword in dwords:
                 if dword and index < 1024:
-                    config_space_map[index] = int(dword, 16)
-                    extended_config_space_map[index] = int(dword, 16)
+                    dword_map[index] = int(dword, 16)
                     index += 1
-    return config_space_map, extended_config_space_map
+    return dword_map
 
-def find_standard_capabilities(config_space_map):
-    standard_capabilities = {}
 
-    capability_start = get_capability_start_offset(config_space_map)
-    current_capability_offset = capability_start
+def locate_caps(dword_map):
+    capabilities = {}
 
-    while current_capability_offset != 0:
-        capability_dword = config_space_map[current_capability_offset // 4]
-        capability_id = (capability_dword >> 24) & 0xFF
-        next_capability_offset = (capability_dword >> 16) & 0xFF
+    start = dword_map[0x34 // 4] >> 24
+    cap_location = start
 
-        standard_capabilities[capability_id] = current_capability_offset
-        current_capability_offset = next_capability_offset
+    while cap_location != 0:
+        cap_dword = dword_map[cap_location // 4]
+        cap_id = (cap_dword >> 24) & 0xFF
+        next_cap = (cap_dword >> 16) & 0xFF
 
-    return standard_capabilities
+        print(f"Found Cap ID: 0x{cap_id:02X}, Start offset: {hex(cap_location)}, Next cap offset: {hex(next_cap)}")
+        capabilities[f"0x{cap_id:02X}"] = cap_location
+        cap_location = next_cap
 
-def find_extended_capabilities(extended_config_space_map):
-    extended_capabilities = {}
 
-    extended_capability_start = get_extended_capability_start_offset(extended_config_space_map)
-    current_extended_capability_offset = extended_capability_start
+    ext_cap_location = 0x100
+    while ext_cap_location != 0:
+        ext_cap_dword = dword_map[ext_cap_location // 4]
 
-    while current_extended_capability_offset != 0:
-        extended_capability_dword = extended_config_space_map[current_extended_capability_offset // 4]
-        extended_capability_id = (extended_capability_dword >> 8) & 0xFF and (extended_capability_dword >> 3) & 0xF
-        extended_capability_id = extended_capability_id << 8 | extended_capability_id << 3
-        next_extended_capability_offset = (extended_capability_dword >> 16) & 0xFF
-        extended_capabilities[extended_capability_id] = current_extended_capability_offset
-        current_extended_capability_offset = next_extended_capability_offset
+        ext_cap_dword_le = int.from_bytes(ext_cap_dword.to_bytes(4, byteorder='big'), byteorder='little')
+        ext_cap_id = ext_cap_dword_le & 0xFFFF
+        next_ext_cap = (ext_cap_dword_le >> 20) & 0xFFF
 
-    return extended_capabilities
+        print(f"Found Ext Cap ID: 0x{ext_cap_id:04X}, Start offset: {hex(ext_cap_location)}, Next cap offset: {hex(next_ext_cap)}")
+        capabilities[f"0x{ext_cap_id:04X}"] = ext_cap_location
+        ext_cap_location = next_ext_cap
 
-def get_capability_start_offset(config_space_map):
-    return config_space_map[0x34 // 4] >> 24
+    return capabilities
 
-def get_extended_capability_start_offset(extended_config_space_map):
-    return extended_config_space_map[0x100 // 4] >> 24
 
-def initialize_writemask(config_space_map):
-    return ['ffffffff' for _ in config_space_map]
+def create_wrmask(dwords):
+    return ['ffffffff' for _ in dwords]
 
-def apply_writemask_update(writemask, update_values, start_index):
-    if not isinstance(update_values, list):
-        raise TypeError("update_values must be a list")
-    end_index = min(start_index + len(update_values), len(writemask))
-    writemask[start_index:end_index] = update_values[:end_index - start_index]
-    return writemask
+
+def update_writemask(wr_mask, input, start_index):
+    end_index = min(start_index + len(input), len(wr_mask))
+    wr_mask[start_index:end_index] = input[:end_index - start_index]
+    return wr_mask
+
 
 def main(file_in, file_out):
-    config_space, extended_config_space = parse_pci_config_space(file_in)
-    standard_capabilities = find_standard_capabilities(config_space)
-    extended_capabilities = find_extended_capabilities(extended_config_space)
-    writemask = initialize_writemask(config_space)
-    
-    # Loop over the fixed_mask_section dictionary
-    for capability_id, update_values in fixed_mask_section.items():
-        writemask = apply_writemask_update(writemask, update_values, 0)
+    cfg_space = read_cfg_space(file_in)
+    caps = locate_caps(cfg_space)
 
-    for capability_id, capability_offset in standard_capabilities.items():
-        writemask_section = fixed_mask_section.get(capability_id)
-        capability_start_index = capability_offset // 4
-        extended_capability_offset = extended_capabilities.get(capability_id)
-        extended_capability_start_index = extended_capability_offset // 4 if extended_capability_offset else None
-        if extended_capability_start_index is not None:
-            writemask = apply_writemask_update(writemask, writemask_section, extended_capability_start_index)
-        writemask = apply_writemask_update(writemask, writemask_section, capability_start_index)
+    wr_mask = create_wrmask(cfg_space)
+    wr_mask = update_writemask(wr_mask, fixed_section, 0)
+
+    for cap_id, cap_start in caps.items():
+        section = writemask_dict.get(cap_id)
+        cap_start_index = cap_start // 4
+        wr_mask = update_writemask(wr_mask, section, cap_start_index)
 
     with open(file_out, 'w') as f:
-        for i in range(0, len(writemask), 4):
-            f.write(','.join(writemask[i:i + 4]) + ',\n')
+        for i in range(0, len(wr_mask), 4):
+            f.write(','.join(wr_mask[i:i + 4]) + ',\n')
 
-    logging_enabled = True
-    if logging_enabled:
-        print(writemask)
-        for index, dword in config_space.items():
-            offset = hex(index * 4)
-            print(f"Index: {index} DWord: {dword}   (offset: {offset} dword {hex(dword)})")
-        for capability_id, capability_offset in standard_capabilities.items():
-            print(f"Cap ID: {hex(capability_id)} offset: {hex(capability_offset)}")
 
 if __name__ == "__main__":
-    main('pcileech_cfgspace.coe', 'pcileech_wrmask.coe')
+    main('pcileech_cfgspace.coe', 'pcileech_cfgspace_writemask.coe')
